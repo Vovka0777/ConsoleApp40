b7da769e4153509f13bf2f395699622ac2e44889
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-25 01:40:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40_4
+{
+    delegate int Sum(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = Summa.Sum;
+
+            Console.Write("Введите первое число - ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число - ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int result = sum(a, b);
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.Write("Введите первое число - ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число - ");
+            b = int.Parse(Console.ReadLine());
+            result = sum(a, b);
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.Write("Введите первое число - ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число - ");
+            b = int.Parse(Console.ReadLine());
+            result = sum(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Summa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40_4
+{
+    class Summa
+    {
+        public static int Sum(int a, int b)
+        {
+            Console.WriteLine($"Сумма чисел от {a} до {b}:");
+            int sum = 0;
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+
+            }
+            return sum;
+        }
+    }
+}
